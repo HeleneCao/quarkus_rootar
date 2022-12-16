dc4e0c3695aa63b1a0309aef878f4c2bebc312d0
HeleneCao <helenecao59@gmail.com>
2022-12-16 11:10:21 +0100

    lastest 16/12/22

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,5 +1,10 @@
 package org.acme;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +13,21 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class ExampleResource {
 
+    @Inject
+    JsonWebToken jwt;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @PermitAll
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("secured")
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed("Admin")
+    public String secureHello(){
+        return "Hello Secured RESTEASY";
     }
 }
